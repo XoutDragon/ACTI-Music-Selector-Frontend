88361bd90c5fb09f1fef57c85815f05470b964ca
Jason Wang <83075516+XoutDragon@users.noreply.github.com>
2022-12-16 08:17:28 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["encrypted-tbn0.gstatic.com"]
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
